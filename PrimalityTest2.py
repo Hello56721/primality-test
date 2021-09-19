730fe0a8ee2d246f6a97babe72a9d677c20e5434
can pooper <82886347+can-pooper@users.noreply.github.com>
2021-09-19 19:04:34 -0400

    Add files via upload

---
+++ PrimalityTest2.py
@@ -0,0 +1,21 @@
+prime = 0
+comp = 0
+c = 0
+i = 2
+while True:
+    is_prime = True
+    for j in range(2, i):
+        if i % j == 0 and j != i:
+            is_prime = False
+            break
+            
+    if is_prime:
+        #print(str(i) + " is prime")
+        prime = prime + 1
+        c = c + 1
+    else:
+        #print(str(i) + " is not prime")
+        comp = comp + 1
+        c = c + 1
+    i = i + 1
+    print("There are " + str(prime) + " primes and " + str(comp) + " composites in the first " + str(c) + " numbers")
